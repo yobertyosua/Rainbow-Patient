a5c3c93957f14458b773f73044539e327b2a2a44
Yobert Yosua <yobertyosua@gmail.com>
2020-03-11 16:07:27 +0100

    Initial commit

---
+++ web/patient-start.js
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+.btn {
+    border: 2px solid black;
+    background-color: white;
+    color: black;
+    padding: 14px 28px;
+    font-size: 16px;
+    cursor: pointer;
+}
+
+/* Lila */
+.schmerzen {
+        background-color: #A020F0;
+        color: white;
+}
+
+.schmerzen:hover {
+        border-color: #A020F0;
+        background-color: white;
+        color: #A020F0;
+}
+
+/* Navy-Blue */
+.hygiene {
+        background-color: #00308F;
+        color: white;
+}
+
+.hygiene:hover {
+        border-color: #00308F;
+        background-color: white;
+        color: #00308F;
+}
+
+/* Orange */
+.medikamente {
+        background-color: #FF7E00;
+        color: white;
+}
+
+.medikamente:hover {
+        border-color: #FF7E00;
+        background-color: white;
+        color: #FF7E00;
+}
+
+/* Türkis */
+.nahrungsmittel {
+        background-color: #08E8DE;
+        color: white;
+}
+
+.nahrungsmittel:hover {
+        border-color: #08E8DE;
+        background-color: white;
+        color: #08E8DE;
+}
+
+/* Grün */
+.sonstiges {
+        background-color: #8DB600;
+        color: white;
+}
+
+.sonstiges:hover {
+        border-color: #8DB600;
+        background-color: white;
+        color: #8DB600;
+}
+</style>
+</head>
+<body>
+
+<h1>Outline Buttons</h1>
+
+<button class="btn schmerzen">Schmerzen</button>
+    <button class="btn hygiene">Hygiene</button>
+    <button class="btn medikamente">Medikamente</button>
+    <button class="btn nahrungsmittel">Nahrungsmittel</button>
+    <button class="btn sonstiges">Sonstiges</button>
+
+    </body>
+    </html>
